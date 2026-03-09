a79e946c85579c91d557ed6d0fc6758b51bc0cb8
Sukram2.0 <webermarkus2008@gmail.com>
2026-03-09 21:11:28 +0100

    Testing the new ordner

--- sidebars.js
+++ sidebars.js
@@ -3,110 +3,41 @@ const sidebars = {
   tutorialSidebar: [
     {
       type: 'category',
-      label: 'Discord Befehle',
-      link: {
-        type: 'generated-index',
-        title: 'Alle Discord Befehle',
-        description: 'Hier findest du eine Übersicht über alle Discord Befehle.',
-        // WICHTIG: Hier KEIN "/docs" reinschreiben! 
-        // Docusaurus macht automatisch "/docs/category/discord/befehle" daraus.
-        slug: '/category/discord/befehle', 
-      },
-      collapsed: false,
+      label: '📦 Admin-Tools',
+      link: { type: 'generated-index', slug: '/admin-tools' },
+      items: [
+        'admin-tools/admin',
+        'admin-tools/roles',
+        'admin-tools/stealemote',
+      ],
+    },
+    {
+      type: 'category',
+      label: '📦 AFK-System',
+      link: { type: 'generated-index', slug: '/afk-system' },
+      items: ['afk-system/afk'],
+    },
+    {
+      type: 'category',
+      label: '📦 Economy',
+      link: { type: 'generated-index', slug: '/economy' },
       items: [
         {
           type: 'category',
-          label: 'Admin Tools',
-          link: {
-            type: 'generated-index',
-            slug: '/category/discord/befehle/admin-tools',
-          },
-          items: [
-            {
-              type: 'category',
-              label: 'Admin-Befehle',
-              link: { type: 'generated-index', slug: '/category/discord/befehle/admin-tools/admin-befehle' },
-              items: [
-                'discord/befehle/Admin Tools/Admin-Befehle/AdminMovechannel',
-                'discord/befehle/Admin Tools/Admin-Befehle/AdminMoverole',
-                'discord/befehle/Admin Tools/Admin-Befehle/AdminSetcategory',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Rollen-Befehle',
-              link: { type: 'generated-index', slug: '/category/discord/befehle/admin-tools/rollen-befehle' },
-              items: [
-                'discord/befehle/Admin Tools/Rollen-Befehle/RolesGive',
-                'discord/befehle/Admin Tools/Rollen-Befehle/RolesRemove',
-                'discord/befehle/Admin Tools/Rollen-Befehle/RolesStatus',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Extra-Befehle',
-              link: { type: 'generated-index', slug: '/category/discord/befehle/admin-tools/extra-befehle' },
-              items: [
-                'discord/befehle/Admin Tools/Emote-Befehle/Stealemote',
-              ],
-            },
-          ],
+          label: 'Coins & Befehle',
+          items: ['economy/economy-befehle/economy'],
         },
         {
           type: 'category',
-          label: 'AFK System',
-          link: { type: 'generated-index', slug: '/category/discord/befehle/afk-system' },
-          items: [
-            'discord/befehle/AFK-System/AFK-Befehle/AFKEnd',
-            'discord/befehle/AFK-System/AFK-Befehle/AFKStart',
-          ],
-        },
-        {
-          type: 'category',
-          label: 'Bewerbungen',
-          link: { type: 'generated-index', slug: '/category/discord/befehle/bewerbungen' },
-          items: ['discord/befehle/Bewerbungen/Apply'],
-        },
-        {
-          type: 'category',
-          label: 'Economy/Wirtschaft',
-          link: { type: 'generated-index', slug: '/category/discord/befehle/economy' },
-          items: [
-            {
-              type: 'category',
-              label: 'Economy-Befehle',
-              link: { type: 'generated-index', slug: '/category/discord/befehle/economy-system/economy-befehle' },
-              items: [
-                'discord/befehle/Economy System/Economy-Befehle/EconomyAdd',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyBalance',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyCrime',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyDaily',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyDeposit',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyDestroy',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyDropDisable',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyDropEnable',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyRemove',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyRob',
-                'discord/befehle/Economy System/Economy-Befehle/EconomySet',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyWeekly',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyWithdraw',
-                'discord/befehle/Economy System/Economy-Befehle/EconomyWork',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Shop-Befehle',
-              link: { type: 'generated-index', slug: '/category/discord/befehle/economy-system/shop-befehle' },
-              items: [
-                'discord/befehle/Economy System/Shop-Befehle/ShopAdd',
-                'discord/befehle/Economy System/Shop-Befehle/ShopBuy',
-                'discord/befehle/Economy System/Shop-Befehle/ShopDelete',
-                'discord/befehle/Economy System/Shop-Befehle/ShopList',
-              ],
-            },
-          ],
+          label: 'Shop System',
+          items: ['economy/shop/shop'],
         },
       ],
+    },
+    {
+      type: 'category',
+      label: '⚙️ Build-In-Commands',
+      items: ['build-in/help', 'build-in/reload'],
     },
   ],
 };
