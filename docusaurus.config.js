38ded3008ad80896ae427c606b8ebfa4f291c50f
Sukram2.0 <webermarkus2008@gmail.com>
2026-04-30 20:52:29 +0200

    Update Google Search Verification

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,6 +50,13 @@ const config = {
   themeConfig:
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
   ({
+    // --- Google Search Console Verifizierung ---
+    metadata: [
+      {
+        name: 'google-site-verification', 
+        content: '_rO_l6Y0O3GlDSuMuum5jFAsMbzkEw9o3gUAmZtI1vw'
+      },
+    ],
     // --- Sidebar Optimierung für Auto-Collapse ---
     docs: {
       sidebar: {
@@ -149,5 +156,4 @@ const config = {
       },
     }),
 };
-
 export default config;
